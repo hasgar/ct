5d7d756c1efe4409525eecea3f4a54a539b62cb5
Hasgar D <hasgardee@gmail.com>
2017-02-03 23:32:57 +0530

    commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -224,7 +224,7 @@ Mail::send('email.newOrder', ['order' => $order, 'cakes' => $cakes, 'user' => $u
           });
        Mail::send('email.invoice', ['order' => $order, 'cakes' => $cakes, 'user' => $user, 'emirate' => $emirate, 'timeslot' => $timeslot], function($message)
           {
-          $message->from('orders@caketreeonline.com')->to($user["email"],'CakeTree Order')
+          $message->from('orders@caketreeonline.com')->to(['hasgardee@gmail.com'],'CakeTree Order')
           ->subject('CakeTree Order Invoice');
         });
          Session::forget('cart');
