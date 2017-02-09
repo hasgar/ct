83a9f680947ed04f598b531eba4eb99c0031e505
Hasgar D <hasgardee@gmail.com>
2017-02-10 00:34:31 +0530

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,7 +47,7 @@ class UserController extends Controller
           'message' => 'required',
         ]);
 
-  Mail::send('email.contact', ['message' => $request["messgae"], 'email' => $request["email"], 'name' => $request["name"]], function($message) use ($request)
+  Mail::send('email.contact', ['message' => $request["message"], 'email' => $request["email"], 'name' => $request["name"]], function($message) use ($request)
      {
      $message->from('orders@caketreeonline.com')->to(['hasgardee@gmail.com'],'Contact form message')->subject('Contact form message');
    });
