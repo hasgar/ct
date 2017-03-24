6fa94e526de2a8970d98ede9c93cd4abc2f78a68
Hasgar D <hasgardee@gmail.com>
2017-03-24 23:45:43 +0530

    commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -208,7 +208,7 @@ return redirect('cake-'.$id.'-view/');
             $destinationPath = 'img/cakes/';
             $fileName = str_replace(" ","_",strtolower($request['name']))."_".$id."_small_".$i.".".$extension;
 
-            if($file->copy(public_path().$destinationPath, $fileName))
+            if($file->copy(public_path().$destinationPath, $fileName))  //
             {
 
             }
